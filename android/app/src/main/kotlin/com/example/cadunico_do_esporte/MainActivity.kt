5c93c5f7574a0fe6de4d957d3c963352b1fad30f
lucaasnogueira <lucs1911@gmail.com>
2023-04-24 23:12:33 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/cadunico_do_esporte/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cadunico_do_esporte
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/loginScreen.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
+        colors: [
+          Colors.white,
+          Colors.white,
+        ],
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: _page(),
+      ),
+    );
+  }
+
+  Widget _page() {
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _icon(),
+            const SizedBox(height: 50),
+            _inputField("Login ID", usernameController),
+            const SizedBox(height: 20),
+            _inputField("Senha", passwordController, isPassword: true),
+            const SizedBox(height: 50),
+            _loginBtn(),
+            const SizedBox(height: 20),
+            _extraText(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _icon() {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Color.fromARGB(255, 6, 31, 75), width: 2),
+          shape: BoxShape.circle),
+      child: const Icon(Icons.person, color: Color.fromARGB(255, 3, 0, 85), size: 120),
+    );
+  }
+
+  Widget _inputField(String hintText, TextEditingController controller,
+      {isPassword = false}) {
+    var border = OutlineInputBorder(
+        borderRadius: BorderRadius.circular(18),
+        borderSide: const BorderSide(color: Color.fromARGB(255, 4, 15, 69)));
+
+    return TextField(
+      style: const TextStyle(color: Color.fromARGB(255, 21, 31, 139)),
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: const TextStyle(color: Color.fromARGB(255, 3, 0, 85)),
+        enabledBorder: border,
+        focusedBorder: border,
+      ),
+      obscureText: isPassword,
+    );
+  }
+
+  Widget _loginBtn() {
+    return ElevatedButton(
+      onPressed: () {
+        debugPrint("Login ID : " + usernameController.text);
+        debugPrint("Senha : " + passwordController.text);
+      },
+      child: const SizedBox(
+          width: double.infinity,
+          child: Text(
+            "Entre",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20),
+          )),
+      style: ElevatedButton.styleFrom(
+        shape: const StadiumBorder(),
+        primary: Colors.grey,
+        onPrimary: Color.fromARGB(255, 0, 21, 57),
+        padding: const EdgeInsets.symmetric(vertical: 16),
+      ),
+    );
+  }
+
+  Widget _extraText() {
+    return const Text(
+      "Esqueceu a senha?",
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 16, color: Color.fromARGB(255, 3, 3, 74)),
+    );
+  }
+}
